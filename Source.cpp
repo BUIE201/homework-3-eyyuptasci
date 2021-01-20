83c4b147f31b46af0587bffa23a4d7957a324638
eyyuptasci <eyyuptasci07@gmail.com>
2021-01-20 15:58:12 +0300

    Some comments are added. We can test much easier now.

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,20 @@
 #include <vector>
 #include <stdlib.h> 
 #include <time.h>
+
+struct Node;
+void InsertToTree(Node*& pRoot, Node* pNew);
+
 struct Node
 {
+public:
 	int i;
 	Node* pLeft;
 	Node* pRight;
 
-	Node(int iIn) : i(iIn), pLeft(nullptr), pRight(nullptr) {}
+	Node(int iIn, Node*& pRoot) : i(iIn), pLeft(nullptr), pRight(nullptr) {
+		InsertToTree(pRoot, this);
+	}
 };
 
 void InsertToTree(Node*& pRoot, Node* pNew)
@@ -44,7 +51,7 @@ void PrintTree(Node* pRoot, int Level)
 	PrintTree(pRoot->pLeft, Level + 1);
 }
 
-void cacheErase(Node*	pErased, std::vector<Node*>& cache) {
+void cacheErase(Node* pErased, std::vector<Node*>& cache) {
 
 	if (pErased->pRight != nullptr)
 		cacheErase(pErased->pRight, cache);
@@ -58,12 +65,16 @@ void cacheErase(Node*	pErased, std::vector<Node*>& cache) {
 }
 
 int BranchWithLargestSumOfNodes(Node* pRoot, std::vector<Node*>& cache) {
+	//This function returns the largest sum of nodes. In the cache vector it holds the nodes that generates that sum.
+	if (pRoot == nullptr)
+		return 0;
 	cache.push_back(pRoot);
 	int sum = pRoot->i;
 	if (pRoot->pLeft != nullptr && pRoot->pRight != nullptr) {
 		int l = BranchWithLargestSumOfNodes(pRoot->pLeft, cache);
 		int r = BranchWithLargestSumOfNodes(pRoot->pRight, cache);
 		sum += l > r ? l : r;
+		//The nodes that generates the less sum are deleted from the cache.
 		l > r ? cacheErase(pRoot->pRight, cache) : cacheErase(pRoot->pLeft, cache);
 	}
 	else if (pRoot->pLeft != nullptr)
@@ -73,7 +84,8 @@ int BranchWithLargestSumOfNodes(Node* pRoot, std::vector<Node*>& cache) {
 	return sum;
 }
 
-void printTheLargestBranch(Node* pRoot, std::vector<Node*> cache) {
+void printTheLargestBranch(Node* pRoot) {
+	std::vector<Node*> cache;
 	int sum = BranchWithLargestSumOfNodes(pRoot, cache);
 	std::cout << "Branch with the largest sum is : ";
 	for (int j = 0; j < cache.size(); j++)
@@ -83,38 +95,15 @@ void printTheLargestBranch(Node* pRoot, std::vector<Node*> cache) {
 
 void main()
 {	
-	srand(time(NULL));
-	std::vector<Node*> nodes;
+	srand(time(NULL)); //I made insertion random since ATÜ removed sorted property.
 
-	//TRIAL LOL
+
+	//TRIAL
 	Node* pRoot = nullptr;
-	Node* p1 = new Node(9);
-	Node* p2 = new Node(10);
-	Node* p9 = new Node(10);
-	Node* p10 = new Node(10);
-	Node* p11 = new Node(10);
-	Node* p12 = new Node(10);
-	Node* p14 = new Node(10);
-	Node* p13 = new Node(10);
-	Node* p3 = new Node(8);
-	Node* p4 = new Node(5);
-	Node* p5 = new Node(6);
-	Node* p6 = new Node(7);
-	Node* p7 = new Node(11);
-	Node* p8 = new Node(12);
-	InsertToTree(pRoot, p1);
-	InsertToTree(pRoot, p2);
-	InsertToTree(pRoot, p3);
-	InsertToTree(pRoot, p4);
-	InsertToTree(pRoot, p5);
-	InsertToTree(pRoot, p6);
-	InsertToTree(pRoot, p10);
-	InsertToTree(pRoot, p9);
-	InsertToTree(pRoot, p11);
-	InsertToTree(pRoot, p12);
-	InsertToTree(pRoot, p13);
-	InsertToTree(pRoot, p7);
-	InsertToTree(pRoot, p8);
-	PrintTree(pRoot,0);
-	printTheLargestBranch(pRoot, nodes);
+	for (int i = 1; i < 40; i++) {
+		new Node(i,pRoot);
+	}
+
+	PrintTree(pRoot,0); // To see what is the tree.
+	printTheLargestBranch(pRoot);
 }
