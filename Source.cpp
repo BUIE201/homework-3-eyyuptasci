07de31953872bc9fb99921638ab038c3cbf0bfa7
eyyuptasci <eyyuptasci07@gmail.com>
2021-01-20 10:51:03 +0300

    Cache is built.

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <stdlib.h> 
 #include <time.h>
 struct Node
@@ -43,25 +44,58 @@ void PrintTree(Node* pRoot, int Level)
 	PrintTree(pRoot->pLeft, Level + 1);
 }
 
-int BranchWithLargestSumOfNodes(Node* pRoot) {
+void cacheErase(Node*	pErased, std::vector<Node*>& cache) {
+
+	if (pErased->pRight != nullptr)
+		cacheErase(pErased->pRight, cache);
+	if (pErased->pLeft != nullptr)
+		cacheErase(pErased->pLeft, cache);
+	
+	auto it = std::find(cache.begin(), cache.end(), pErased);
+	if(it != cache.end())
+		cache.erase(it);
+			
+}
+
+int BranchWithLargestSumOfNodes(Node* pRoot, std::vector<Node*>& cache) {
+	cache.push_back(pRoot);
 	int sum = pRoot->i;
-	if (pRoot->pLeft != nullptr && pRoot->pRight != nullptr)
-		sum += BranchWithLargestSumOfNodes(pRoot->pLeft) > BranchWithLargestSumOfNodes(pRoot->pRight) ? BranchWithLargestSumOfNodes(pRoot->pLeft) : BranchWithLargestSumOfNodes(pRoot->pRight);
+	if (pRoot->pLeft != nullptr && pRoot->pRight != nullptr) {
+		int l = BranchWithLargestSumOfNodes(pRoot->pLeft, cache);
+		int r = BranchWithLargestSumOfNodes(pRoot->pRight, cache);
+		sum += l > r ? l : r;
+		l > r ? cacheErase(pRoot->pRight, cache) : cacheErase(pRoot->pLeft, cache);
+	}
 	else if (pRoot->pLeft != nullptr)
-		sum += BranchWithLargestSumOfNodes(pRoot->pLeft);
+		sum += BranchWithLargestSumOfNodes(pRoot->pLeft, cache);
 	else if (pRoot->pRight != nullptr)
-		sum += BranchWithLargestSumOfNodes(pRoot->pRight);
+		sum += BranchWithLargestSumOfNodes(pRoot->pRight, cache);
 	return sum;
+}
+
+void printTheLargestBranch(Node* pRoot, std::vector<Node*> cache) {
+	int sum = BranchWithLargestSumOfNodes(pRoot, cache);
+	std::cout << "Branch with the largest sum is : ";
+	for (int j = 0; j < cache.size(); j++)
+		std::cout << cache.at(j)->i << " ";
+	std::cout << "-> SUM = " << sum;
 }
 
 void main()
 {	
 	srand(time(NULL));
-	int i;
+	std::vector<Node*> nodes;
 
+	//TRIAL LOL
 	Node* pRoot = nullptr;
 	Node* p1 = new Node(9);
 	Node* p2 = new Node(10);
+	Node* p9 = new Node(10);
+	Node* p10 = new Node(10);
+	Node* p11 = new Node(10);
+	Node* p12 = new Node(10);
+	Node* p14 = new Node(10);
+	Node* p13 = new Node(10);
 	Node* p3 = new Node(8);
 	Node* p4 = new Node(5);
 	Node* p5 = new Node(6);
@@ -74,8 +108,13 @@ void main()
 	InsertToTree(pRoot, p4);
 	InsertToTree(pRoot, p5);
 	InsertToTree(pRoot, p6);
+	InsertToTree(pRoot, p10);
+	InsertToTree(pRoot, p9);
+	InsertToTree(pRoot, p11);
+	InsertToTree(pRoot, p12);
+	InsertToTree(pRoot, p13);
 	InsertToTree(pRoot, p7);
 	InsertToTree(pRoot, p8);
 	PrintTree(pRoot,0);
-	std::cout << BranchWithLargestSumOfNodes(pRoot);
+	printTheLargestBranch(pRoot, nodes);
 }
